378270d418d0af20f5ce417bb671b478f687099f
Somnath Mahato <somnathmahato723@gmail.com>
2025-02-04 01:36:01 +0530

    hoisting basics covered

---
+++ javascript-practise/hoisting-javascript.js
@@ -0,0 +1,13 @@
+const myName = "Somnath"
+
+if (myName === "Somnath") {
+    console.log(`Somnath is a ${job}`);
+    const age = 2025 - 1993;
+    console.log(age);
+    var job = 'Software Engineer';
+    console.log(x);
+}
+
+console.log("Done!");
+
+
